644a0bf4ff4ea59783d6f9ecaf4e65961c3d21a5
tav <tav@espians.com>
2010-03-18 11:14:25 +0000

    Added support for nodelint to lint itself.

--- nodelint.js
+++ nodelint.js
@@ -11,7 +11,7 @@
  */
 
 /*global JSLINT */
-/*jslint evil: true */
+/*jslint evil: true, regexp: false */
 
 var fs = require('fs'),
     sys = require('sys');
@@ -72,10 +72,12 @@ function main() {
         sys.puts(err);
         process.exit(1);
     }
+    // remove any shebangs
+    source = source.replace(/^\#\!.*/, '');
     if (!JSLINT(source, {bitwise: true, eqeqeq: true, immed: true,
                 newcap: true, nomen: false, onevar: true, plusplus: true,
                 predef: ['exports', 'module', 'require', 'process', '__filename', 'GLOBAL'],
-                regexp: true, rhino: false, undef: true, white: true, indent: 2})) {
+                regexp: true, rhino: false, undef: true, white: true})) {
         for (i = 0; i < JSLINT.errors.length; i += 1) {
             error = JSLINT.errors[i];
             if (error) {
