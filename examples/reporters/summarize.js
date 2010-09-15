03a7afc3e0a891555ab25cce5e4b6cc82b78a563
tav <tav@espians.com>
2010-09-15 02:57:19 +0100

    Updated all files to pass with new indent level of 2.

--- examples/reporters/summarize.js
+++ examples/reporters/summarize.js
@@ -5,6 +5,6 @@
 var sys = require('sys');
 
 function reporter(results) {
-    var len = results.length;
-    sys.puts(len + ' error' + ((len === 1) ? '' : 's'));
+  var len = results.length;
+  sys.puts(len + ' error' + ((len === 1) ? '' : 's'));
 }
--- examples/reporters/xml.js
+++ examples/reporters/xml.js
@@ -9,21 +9,25 @@ var path = require('path'),
     sys = require('sys');
 
 function reporter(results) {
-    function escape(str) {
-        return (str) ? str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;') : '';
-    }
 
-    var i, error, len, file,
-        dir = process.cwd(),
-        xml = '<?xml version="1.0" encoding="UTF-8" ?>\n<jslint>\n';
-    for (i = 0, len = results.length; i < len; i += 1) {
-        file = path.join(dir, results[i].file);
-        error = results[i].error;
-        xml += '\t<file name="' + file + '">\n' +
-                '\t\t<issue char="' + error.character + '" evidence="' + escape(error.evidence || '') +
-                '" line="' + error.line + '" reason="' + escape(error.reason) + '"/>\n' +
-                '\t</file>\n';
-    }
-    xml += '</jslint>';
-    sys.puts(xml);
+  function escape(str) {
+    return (str) ? str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;') : '';
+  }
+
+  var i, error, len, file,
+      dir = process.cwd(),
+      xml = '<?xml version="1.0" encoding="UTF-8" ?>\n<jslint>\n';
+
+  for (i = 0, len = results.length; i < len; i += 1) {
+    file = path.join(dir, results[i].file);
+    error = results[i].error;
+    xml += '\t<file name="' + file + '">\n' +
+           '\t\t<issue char="' + error.character + '" evidence="' + escape(error.evidence || '') +
+           '" line="' + error.line + '" reason="' + escape(error.reason) + '"/>\n' +
+           '\t</file>\n';
+  }
+
+  xml += '</jslint>';
+  sys.puts(xml);
+
 }
