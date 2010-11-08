9ff98a5e8614ceeafd2db1213f0d2a417ccec345
Julien Polo <julien.polo@altshift.fr>
2010-11-08 14:00:27 +0100

    * various bugs corrected

--- test/lint/formatter/cli_test.js
+++ test/lint/formatter/cli_test.js
@@ -24,29 +24,29 @@ function createReport() {
 
 var FormatterTest = vows.describe('Cli Formatter class').addBatch({
     "format() / mode=simple" : {
-    topic : function (item) {
-    return createFormatter({mode: 'simple'});
-},
-'should return ' : function (topic) {
-    assert.equal(topic.format(createReport()), '1 error\n');
-}
-},
-"format() / mode=normal" : {
-    topic : function (item) {
-    return createFormatter({mode: 'normal'});
-},
-'should return ' : function (topic) {
-    assert.equal(topic.format(createReport()), 'foo: line 12, character 5 r\ne\n1 error\n');
-}
-},
-"format() / mode=full" : {
-    topic : function (item) {
-    return createFormatter({mode: 'full'});
-},
-'should return ' : function (topic) {
-    assert.equal(topic.format(createReport()), 'foo: line 12, character 5, r\ne\n1 error\n');
-}
-}
+        topic : function (item) {
+            return createFormatter({mode: 'simple'});
+        },
+        'should return ' : function (topic) {
+            assert.equal(topic.format(createReport()), '1 error\n');
+        }
+        },
+        "format() / mode=normal" : {
+            topic : function (item) {
+            return createFormatter({mode: 'normal'});
+        },
+        'should return ' : function (topic) {
+            assert.equal(topic.format(createReport()), 'foo: line 12, character 5 r\ne\n1 error\n');
+        }
+        },
+        "format() / mode=full" : {
+            topic : function (item) {
+            return createFormatter({mode: 'full'});
+        },
+        'should return ' : function (topic) {
+            assert.equal(topic.format(createReport()), 'foo: line 12, character 5, r\ne\n1 error\n');
+        }
+    }
 });
 
 exports.FormatterTest = FormatterTest;
--- test/lint/formatter/vim_test.js
+++ test/lint/formatter/vim_test.js
@@ -8,30 +8,30 @@ function createFormatter(options) {
 
 function createReport() {
     return [
-            {
-                file : 'foo',
-                error: {
+        {
+            file : 'foo',
+            error: {
                 line: 12,
                 character: 5,
                 evidence: 'e',
                 reason: 'r'
             }
-            }
-            ];
+        }
+    ];
 }
 
 var FormatterTest = vows.describe('VIM Formatter class').addBatch({
     "format()" : {
-    topic : function (item) {
-    return createFormatter();
-},
-'should format empty data' : function (topic) {
-    assert.equal(topic.format([]), '');
-},
-'should format simple data' : function (topic) {
-    assert.equal(topic.format(createReport()), 'foo line 12 column 5 Error: r e');
-}
-}
+        topic : function (item) {
+            return createFormatter();
+        },
+        'should format empty data' : function (topic) {
+            assert.equal(topic.format([]), '');
+        },
+        'should format simple data' : function (topic) {
+            assert.equal(topic.format(createReport()), 'foo line 12 column 5 Error: r e');
+        }
+    }
 });
 
 exports.FormatterTest = FormatterTest;
